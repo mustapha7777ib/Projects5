f3cd67e9042768955004ab07c0e36f77bef55f64
Mustapha Ibrahim <ibrahimmustapha12345678@gmail.com>
2024-05-10 10:04:30 +0100

    third commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var images = document.querySelectorAll('.a1 img');
+
+images.forEach(function(image) {
+    image.addEventListener('click', display);
+});
+
+function display(event) {
+    var extendsDiv = event.target.parentElement.nextElementSibling; 
+    let height = extendsDiv.children[0].offsetHeight;
+    console.log(height);
+    if (extendsDiv.style.maxHeight === `${height + 20}px`) {
+        extendsDiv.style.maxHeight = 0; 
+        event.target.src = "./assets/images/icon-plus.svg";
+    } else {
+        images.forEach(function(image) {
+            image.parentElement.nextElementSibling.style.maxHeight = 0;
+            image.src = "./assets/images/icon-plus.svg";
+        })
+        extendsDiv.style.maxHeight = `${height + 20}px`;
+        event.target.src = "./assets/images/icon-minus.svg";
+    }
+}
